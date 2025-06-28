98f59be5cf9644703052c04416659a783a1a9514
Piyush Saxena <piyushsaxena2911@gmail.com>
2025-06-28 21:50:49 +0530

    Added Code Files

---
+++ container folder/container.tf
@@ -0,0 +1,7 @@
+resource "azurerm_storage_container" "baran3432" {
+  name                  = "falana1"
+  storage_account_id    =  "subscriptions/1f43e7c1-4e8a-4da0-877f-76b7cb3e5898/resourceGroups/rajat454561/providers/Microsoft.Storage/storageAccounts/rani453611/"
+
+
+  container_access_type = "private"
+}
---
+++ prod/prod.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+  #   backend "azurerm" {
+  #     resource_group_name  = "rajat45456"
+  #     storage_account_name = "rani45361"               # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+  #     container_name       = "falana1"                 # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+  #     key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  #   }
+}
+
+
+
+provider "azurerm" {
+  features {}
+  subscription_id = "1f43e7c1-4e8a-4da0-877f-76b7cb3e5898"
+}
